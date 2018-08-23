330b011923277cf104783e7ec65647bc72b9455d
Antonie Lin <iammrhelo@gmail.com>
2018-08-23 13:28:02 -0700

    rm unused and demo app

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name="ImageEditDialogueSelfPlay",
-    version='0.1dev',
-    packages=['iedsp'],
-    long_description=open("README.md", "r").read()
-)
